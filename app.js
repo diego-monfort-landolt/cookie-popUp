494678c5b44824e86b4256e9c0f5f3027a776f5f
Diego19890102 <diegomonfort1989@gmail.com>
2023-05-13 14:16:12 +0200

    .
    .

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //gsap.fromTo('.text', {opacity: 0, y: 30}, {opacity: 1, y: 0, duration: 1})
 const tl = gsap.timeline({defaults: {duration: 1.5, ease: "power1.out"}})
-
-tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic,out(1, 0.4)", duration: 1.5})
+//
+tl.fromTo('.cookie-container', {scale: 0}, {scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5})
 tl.fromTo('.cookie', {opacity: 0, x: -50, rotation: '-55deg'}, {opacity: 1, x: 0, rotation: '0deg'}, '<50%')
 tl.fromTo('.text', {x: 30, opacity: 0}, {x: 0, opacity: 1}, '<')
 
+//cookie jump
+tl.fromTo('.cookie', {y:0}, {y:-20, yoyo: true, repeate: -1})
+//fading the Cookie out
 const button = document.querySelector('button');
 button.addEventListener('click', () => {
     gsap.to('.cookie-container', {opacity: 0, y: 100, duration: 0.75, ease: 'power1.out'} )
